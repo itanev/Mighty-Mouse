8c9ff1a5479ac64c7daed1138392891634ff6e01
itanev <iliyan.tanev@gmail.com>
2013-10-26 01:19:45 +0300

    Project Base

---
+++ eDoc/eDoc.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eDoc.Web.MvcApplication" Language="C#" %>
---
+++ eDoc/eDoc.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eDoc.Web.Startup))]
+namespace eDoc.Web
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
