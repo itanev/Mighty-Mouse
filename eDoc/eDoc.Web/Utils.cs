1c08d793f7d3a9e608a49e63ac8d36fd5db992e6
s-rusev <rusev.s@gmail.com>
2013-10-26 19:33:30 +0300

    cleanup

---
+++ eDoc/eDoc.Web/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eDoc.Web
+{
+    public class Utils
+    {
+        public static string GetConfirmationCode(object seed, int length)
+        {
+            var random = new Random(seed.GetHashCode() + Environment.TickCount);
+            var array = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = (char)random.Next((int)'a', (int)'z');
+            }
+            return new string(array);
+        }
+    }
+}
