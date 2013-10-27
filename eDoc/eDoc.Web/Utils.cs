edc0d28d8a10cfd8a6cc25ad64b04f095c91cadb
staafl <velko.nikolov@gmail.com>
2013-10-27 11:13:38 +0200

    added token verification helpers

--- eDoc/eDoc.Web/Utils.cs
+++ eDoc/eDoc.Web/Utils.cs
@@ -7,15 +7,35 @@ namespace eDoc.Web
 {
     public class Utils
     {
-        public static string GetConfirmationCode(object seed, int length)
+        public static string GetConfirmationCode(object seed, int length, bool useEnvTickCount = true)
         {
-            var random = new Random(seed.GetHashCode() + Environment.TickCount);
-            var array = new char[length];
-            for (int i = 0; i < length; i++)
-            {
-                array[i] = (char)random.Next((int)'a', (int)'z');
-            }
-            return new string(array);
+            int seedInt = seed.GetHashCode();
+            if (useEnvTickCount) seedInt += Environment.TickCount;
+            var random = new Random(seedInt);
+            var array = new byte[length];
+            random.NextBytes(array);
+            return Convert.ToBase64String(array).Substring(0, length);
         }
+        
+        public static string GetTokenConfirmationCode(string code) {
+            return GetConfirmationCode(code, code.Length, false);
+        }
+        
+        public const string GetTokenConfirmationCodeSource =
+@"public static string GetConfirmationCode(string code)
+{
+    var random = new Random(code.GetHashCode());
+    var array = new byte[code.Length];
+    random.NextBytes(array);
+    return Convert.ToBase64String(array).Substring(0, length);
+}
+";
     }
 }
+
+
+
+
+
+
+
